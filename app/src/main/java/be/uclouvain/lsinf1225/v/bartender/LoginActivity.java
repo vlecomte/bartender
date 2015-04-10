5a11ffac73fd06245f765e99b96e2f6ae7202047
Victor Lecomte <victor.lecomte@student.uclouvain.be>
2015-04-10 22:25:18 +0200

    Ajouté des commentaires et le titre de l'application

--- app/src/main/java/be/uclouvain/lsinf1225/v/bartender/LoginActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/v/bartender/LoginActivity.java
@@ -11,7 +11,7 @@ import android.widget.Toast;
 
 
 /**
- * A login screen that offers login via username/password.
+ * A login screen that switches to a register screen if the username does not exist yet.
  */
 public class LoginActivity extends Activity {
 
@@ -29,7 +29,7 @@ public class LoginActivity extends Activity {
     private EditText mConfirmPasswordView, mEmailView;
     // Buttons.
     private Button mSignInButton, mRegisterButton;
-
+    // Whether the registering part is shown.
     private boolean registerShown = false;
 
     @Override
@@ -60,8 +60,10 @@ public class LoginActivity extends Activity {
         });
     }
 
+    /**
+     * Shows the registering part of the form.
+     */
     private void showRegister() {
-
         registerShown = true;
 
         // Hide login button
@@ -76,8 +78,10 @@ public class LoginActivity extends Activity {
         mConfirmPasswordView.requestFocus();
     }
 
+    /**
+     * Hides the registering part of the form.
+     */
     private void hideRegister() {
-
         registerShown = false;
 
         // Hide additional fields and register button.
@@ -104,8 +108,7 @@ public class LoginActivity extends Activity {
 
     /**
      * Attempts to sign in or register the account specified by the login form.
-     * If there are form errors (invalid email, missing fields, etc.), the
-     * errors are presented and no actual login attempt is made.
+     * If some of the fields are invalid, an error is reported and no actual attempt is made.
      */
     public void attemptLogin() {
 
@@ -138,6 +141,10 @@ public class LoginActivity extends Activity {
         userLogin(username, password);
     }
 
+    /**
+     * Attempts to register the account specified by the login form.
+     * If some of the fields are invalid, an error is reported and no actual attempt is made.
+     */
     private void attemptRegister() {
 
         resetErrors();
@@ -189,12 +196,20 @@ public class LoginActivity extends Activity {
         userRegister(username, password, email);
     }
 
-    private void userLogin(String email, String password) {
+    /**
+     * Attempts to sign in with the specified credentials.
+     * If the username does not exist, the form switches to register mode.
+     * If the password is incorrect, an error is reported.
+     * Otherwise, we switch to the main menu.
+     * @param username The username of the account to be accessed.
+     * @param password The password to be checked.
+     */
+    private void userLogin(String username, String password) {
 
         boolean exists = false, success = false;
         for (String credential : DUMMY_CREDENTIALS) {
             String[] pieces = credential.split(":");
-            if (pieces[0].equals(email)) {
+            if (pieces[0].equals(username)) {
                 exists = true;
                 success = pieces[1].equals(password);
             }
@@ -209,6 +224,14 @@ public class LoginActivity extends Activity {
         }
     }
 
+    /**
+     * Attempts to register a new user with the specified credentials.
+     * If the username already exists, an error is reported.
+     * Otherwise, we switch to the main menu.
+     * @param username The username of the account to be created.
+     * @param password The password to be associated with the account.
+     * @param email The email address to be associated with the account.
+     */
     private void userRegister(String username, String password, String email) {
         // TODO: Register user
         Toast.makeText(this, "Registering ("+username+", "+password+", "+email+").",
@@ -216,26 +239,46 @@ public class LoginActivity extends Activity {
         // TODO: Switch to main menu
     }
 
+    /**
+     * Checks that a username has a valid format.
+     * @param username The username to be checked.
+     * @return true if the username is valid, false otherwise.
+     */
     private boolean isUsernameValid(String username) {
         return username.length() >= 3 && username.length() <= 50;
     }
 
+    /**
+     * Checks that a password has a valid format.
+     * @param password The password to be checked.
+     * @return true if the password is valid, false otherwise.
+     */
     private boolean isPasswordValid(String password) {
         return password.length() >= 5 && password.length() <= 50;
     }
 
+    /**
+     * Checks that an email address has a valid format
+     * @param email The email address to be checked.
+     * @return true if the email address is valid, false otherwise.
+     */
     private boolean isEmailValid(String email) {
         // TODO: Find out an easy way to check an email
         return email.contains("@");
     }
 
+    /**
+     * Reports an error on a field and moves the focus to it.
+     * @param field The field that has caused an error.
+     * @param message The error message to be displayed.
+     */
     private void reportError(EditText field, String message) {
         field.setError(message);
         field.requestFocus();
     }
 
     /**
-     * Resets errors on all fields.
+     * Removes the error messages on all fields.
      */
     private void resetErrors() {
         mUsernameView.setError(null);
