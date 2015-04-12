2a1ff65a1004350982945437b0f91a9215473831
Victor Lecomte <victor.lecomte@student.uclouvain.be>
2015-04-13 01:24:11 +0200

    Le bouton log out est maintenant fonctionnel

--- app/src/main/java/be/uclouvain/lsinf1225/v/bartender/MainActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/v/bartender/MainActivity.java
@@ -1,8 +1,10 @@
 package be.uclouvain.lsinf1225.v.bartender;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 
@@ -19,8 +21,25 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
+        // Inflate the menu items for use in the action bar.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items.
+        switch (item.getItemId()) {
+
+            case R.id.action_logout:
+                MyApp.logoutUser();
+                Intent intent = new Intent(this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
