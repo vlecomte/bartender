224bae7e08f92e89ce8d6080c558752c0f1ec692
Momin Charles <charles.momin@student.uclouvain.be>
2015-04-27 12:55:44 +0200

    Ajout de fonctionnalité: confirmer commande

--- app/src/main/java/be/uclouvain/lsinf1225/v/bartender/BasketShowActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/v/bartender/BasketShowActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import android.app.Activity;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Button;
 import android.widget.Toast;
@@ -24,6 +26,8 @@ public class BasketShowActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_basket_show);
+        findViewById(R.id.radioTable).setVisibility(View.INVISIBLE);
+        findViewById(R.id.confirm_table_button).setVisibility(View.INVISIBLE);
         Map<Product, Integer> basket_elem = MyApp.getCustomer().getBasket();
         String[] tab_elem = new String[basket_elem.size()];
         Double[] tab_prix_elem = new Double[basket_elem.size()];
@@ -44,16 +48,38 @@ public class BasketShowActivity extends Activity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //CONFIRMATION PANIER
-                //MyApp.getCustomer().clearBasket();
-                Intent intent = new Intent(BasketShowActivity.this, MainActivity.class);
-                startActivity(intent);
-                Toast.makeText(BasketShowActivity.this, R.string.confirmed_basket, Toast.LENGTH_LONG).show();
-
+                if(MyApp.getCustomer().hasCurrentOrder()){
+                    MyApp.getCustomer().confirmBasket();
+                    Intent intent = new Intent(BasketShowActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(BasketShowActivity.this, R.string.confirmed_basket, Toast.LENGTH_LONG).show();
+                } else {
+                    showSetTableLayout();
+                    Button but = (Button) findViewById(R.id.confirm_table_button);
+                    but.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            RadioGroup listTable = (RadioGroup) findViewById(R.id.radioTable);
+                            RadioButton tableChoice = (RadioButton) findViewById(listTable.getCheckedRadioButtonId());
+                            int table = Integer.parseInt(tableChoice.getText().toString());
+                            MyApp.getCustomer().openOrder(table);
+                            Intent intent = new Intent(BasketShowActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            Toast.makeText(BasketShowActivity.this, R.string.confirmed_basket, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
             }
         });
     }
-
+    public void showSetTableLayout() {
+        findViewById(R.id.listView_basket).setVisibility(View.INVISIBLE);
+        ((TextView) findViewById(R.id.basket_ss)).setText(R.string.choose_table);
+        findViewById(R.id.total_basket).setVisibility(View.INVISIBLE);
+        findViewById(R.id.confirm_basket_button).setVisibility(View.INVISIBLE);
+        findViewById(R.id.radioTable).setVisibility(View.VISIBLE);
+        ((Button) findViewById(R.id.confirm_table_button)).setVisibility(View.VISIBLE);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
