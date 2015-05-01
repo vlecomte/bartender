c561d50e37c4f99b8dc4e44ef88e1a4b762811c1
Momin Charles <charles.momin@student.uclouvain.be>
2015-05-02 01:30:19 +0200

    "affichage des commandes par tables [coté serveur]"

--- app/src/main/java/be/uclouvain/lsinf1225/v/bartender/gui/ToServeFragment.java
+++ app/src/main/java/be/uclouvain/lsinf1225/v/bartender/gui/ToServeFragment.java
@@ -9,6 +9,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.List;
@@ -22,77 +23,77 @@ import be.uclouvain.lsinf1225.v.bartender.util.CustomDetList;
 import be.uclouvain.lsinf1225.v.bartender.util.CustomListTable;
 
 public class ToServeFragment extends Fragment {
-    ListView tableList;
-    ListView detailsList;
-    Button annuleButton;
-    boolean hide = true;
+    ListView Tables;
+    ListView Details;
+    Button annule;
+    Button Confirmer;
+    TextView text;
+    View v;
+    boolean choice_table;
 
-    String[] table;
-    Integer[] nbr_details;
-    String[] descDetails;
-    List<Detail>[] detList;
+    String[] tabDestDescr;
 
     @Override
     public View onCreateView(LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         final View view = inflater.inflate(R.layout.activity_show_details, container, false);
-        loadTabs(DaoDetail.getOpenByTable());
-        tableList = (ListView) view.findViewById(R.id.tableList);
-        detailsList = (ListView) view.findViewById(R.id.detailList);
-        CustomListTable adapter = new CustomListTable(getActivity(), table, nbr_details);
-        tableList.setAdapter(adapter);
-        tableList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                UpdateView();
-                generateTabFromList(detList[position]);
-                detailsList.setAdapter(new CustomDetList(getActivity(),descDetails));
-            }
-        });
-        annuleButton = (Button) view.findViewById(R.id.action_annule_button);
-        annuleButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UpdateView();
-            }
-        });
-        annuleButton.setVisibility(View.INVISIBLE);
+        v=view;
+        choice_table=true;
+        UpdateView();
+        updateLayout();
         return view;
     }
 
-    private void loadTabs(Map<Integer, List<Detail>> map){
-        int i=0;
-        table = new String[map.size()];
-        detList = new List[map.size()];
-        nbr_details = new Integer[map.size()];
-        for (Map.Entry<Integer, List<Detail>> entry : map.entrySet()){
-            table[i]= "table n°"+entry.getKey();
-            List<Detail> liste = entry.getValue();
-            nbr_details[i] = liste.size();
-            detList[i]=liste;
-            i++;
-        }
+    private void generateTabFromList(List<Detail> list, String[] tab){
+            for(int i=0;i<list.size();i++){
+                tab[i] = list.get(i).getProduct().getDisplayName();
+            }
     }
 
-    private void generateTabFromList(List<Detail> list){
-        descDetails = new String[list.size()];
-        for (int i =0;i<list.size();i++){
-            System.out.println("VALEUR="+list.get(i).getProduct().getDisplayName());
-            descDetails[i] = list.get(i).getProduct().getDisplayName();
+    private void updateLayout(){
+        if(choice_table) {
+            Details.setVisibility(View.INVISIBLE);
+            annule.setVisibility(View.INVISIBLE);
+            Tables.setVisibility(View.VISIBLE);
+            choice_table = false;
+        } else {
+            Details.setVisibility(View.VISIBLE);
+            annule.setVisibility(View.VISIBLE);
+            Tables.setVisibility(View.INVISIBLE);
+            choice_table = true;
         }
     }
 
     private void UpdateView(){
-        if(hide){
-            tableList.setVisibility(View.INVISIBLE);
-            annuleButton.setVisibility(View.VISIBLE);
-            detailsList.setVisibility(View.VISIBLE);
-            hide = false;
-        } else {
-            tableList.setVisibility(View.VISIBLE);
-            annuleButton.setVisibility(View.INVISIBLE);
-            detailsList.setVisibility(View.INVISIBLE);
-            hide = true;
+        Tables = (ListView) v.findViewById(R.id.tableList);
+        Details = (ListView) v.findViewById(R.id.detailList);
+        annule = (Button) v.findViewById(R.id.action_annule_button);
+        annule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateLayout();
+            }
+        });
+        Map<Integer, List<Detail>> liste = DaoDetail.getOpenByTable();
+        String[] tabTables = new String[liste.size()];
+        Integer[] tabNbrDetByTable = new Integer[liste.size()];
+        final List[] tabList = new List[liste.size()];
+        int i=0;
+        for(Map.Entry<Integer, List<Detail>> entry : liste.entrySet()) {
+            tabTables[i] = "Table n°" + entry.getKey();
+            tabNbrDetByTable[i] = entry.getValue().size();
+            tabList[i] = entry.getValue();
+            i++;
         }
+        Tables.setAdapter(new CustomListTable(getActivity(), tabTables,tabNbrDetByTable));
+        Tables.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                tabDestDescr = new String[tabList[position].size()];
+                generateTabFromList(tabList[position],tabDestDescr);
+                Details.setAdapter(new CustomDetList(getActivity(), tabDestDescr));
+                updateLayout();
+            }
+        });
     }
 }
