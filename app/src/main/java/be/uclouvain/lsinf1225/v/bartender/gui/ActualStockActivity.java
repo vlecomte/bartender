38ef88a69be3903f420a5d1f940ecf21ac24749c
Harold Somers <haroldsomers@MacBook-Pro-de-Harold-2.local>
2015-05-05 01:29:36 +0200

    Ajout de Stock Actuel

---
+++ app/src/main/java/be/uclouvain/lsinf1225/v/bartender/gui/ActualStockActivity.java
@@ -0,0 +1,7 @@
+package be.uclouvain.lsinf1225.v.bartender.gui;
+
+/**
+ * Created by haroldsomers on 5/05/15.
+ */
+public class ActualStockActivity {
+}
---
+++ app/src/main/java/be/uclouvain/lsinf1225/v/bartender/util/CustomActualStock.java
@@ -0,0 +1,7 @@
+package be.uclouvain.lsinf1225.v.bartender.util;
+
+/**
+ * Created by haroldsomers on 5/05/15.
+ */
+public class CustomActualStock {
+}
