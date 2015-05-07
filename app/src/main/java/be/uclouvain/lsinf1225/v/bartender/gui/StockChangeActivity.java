cb761adc115932103f3cc58e5c99108335249ba1
Harold Somers <haroldsomers@MacBook-Pro-de-Harold-2.local>
2015-05-07 21:30:55 +0200

    Ajout de ChangeStockActivity 2

--- app/src/main/java/be/uclouvain/lsinf1225/v/bartender/gui/StockChangeActivity.java
+++ app/src/main/java/be/uclouvain/lsinf1225/v/bartender/gui/StockChangeActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import be.uclouvain.lsinf1225.v.bartender.R;
 import be.uclouvain.lsinf1225.v.bartender.dao.DaoIngredient;
@@ -25,9 +26,15 @@ public class StockChangeActivity extends Activity {
     Ingredient ingredient;
     TextView name_ingredient;
     TextView stock_actuel;
-    EditText quantite;
+    TextView stock_critical;
+    TextView stock_max;
+    EditText quantite1;
+    EditText quantite2;
+    EditText quantite3;
     Button ajouter;
     Button retirer;
+    Button modifier_max;
+    Button modifier_critical;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,24 +42,34 @@ public class StockChangeActivity extends Activity {
         ingredient = MyApp.getIngredientToChange();
         name_ingredient = (TextView) findViewById(R.id.txt_change_stock);
         stock_actuel = (TextView) findViewById(R.id.txt_actual_stock);
-        quantite = (EditText) findViewById(R.id.edit_actual_stock);
+        stock_critical = (TextView) findViewById(R.id.txt_critical_stock);
+        stock_max = (TextView) findViewById(R.id.txt_max_stock);
+        quantite1 = (EditText) findViewById(R.id.edit_actual_stock);
+        quantite2 = (EditText) findViewById(R.id.edit_critical_stock);
+        quantite3 = (EditText) findViewById(R.id.edit_max_stock);
         ajouter = (Button) findViewById(R.id.button1_actual_stock);
         retirer = (Button) findViewById(R.id.button2_actual_stock);
+        modifier_critical = (Button) findViewById(R.id.button1_critical_stock);
+        modifier_max = (Button) findViewById(R.id.button1_max_stock);
 
 
         name_ingredient.setText(ingredient.getDisplayName());
         stock_actuel.setText("Actuel: " + ingredient.getCurrent());
+        stock_critical.setText("Seuil: " + ingredient.getCritical());
+        stock_max.setText("Max: " + ingredient.getMax());
 
         ajouter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String t = quantite.getText().toString();
+                String t = quantite1.getText().toString();
                 Double q= ingredient.getCurrent() + (double)Float.valueOf(t);
                 if (q>ingredient.getMax()){
-
+                    ingredient.setCurrent(ingredient.getMax());
+                    stock_actuel.setText("Actuel: " + ingredient.getCurrent());
+                    Toast.makeText(StockChangeActivity.this, "Stock au max", Toast.LENGTH_LONG).show();
                 }
                 else{
-                    DaoIngredient.setCurrent(ingredient.getDisplayName(),q);
+                    ingredient.setCurrent(q);
                     ingredient = MyApp.getIngredientToChange();
                     stock_actuel.setText("Actuel: " + ingredient.getCurrent());
                 }
@@ -64,19 +81,39 @@ public class StockChangeActivity extends Activity {
             @Override
             public void onClick(View v) {
 
-                String t = quantite.getText().toString();
+                String t = quantite1.getText().toString();
                 Double q= ingredient.getCurrent() - (double)Float.valueOf(t);
                 if (q<0){
-
+                    ingredient.setCurrent(0.0);
+                    stock_actuel.setText("Actuel: " + ingredient.getCurrent());
+                    Toast.makeText(StockChangeActivity.this, "Stock insuffisant", Toast.LENGTH_LONG).show();
                 }
                 else{
-                    DaoIngredient.setCurrent(ingredient.getDisplayName(),q);
-                    ingredient = DaoIngredient.getByName(ingredient.getName());
+                    ingredient.setCurrent(q);
                     stock_actuel.setText("Actuel: " + ingredient.getCurrent());
                 }
             }
         });
 
+        modifier_critical.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String t = quantite2.getText().toString();
+                Double q= (double)Float.valueOf(t);
+                ingredient.setCritical(q);
+                stock_critical.setText("Seuil: " + ingredient.getCritical());
+            }
+        });
+
+        modifier_max.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String t = quantite3.getText().toString();
+                Double q= (double)Float.valueOf(t);
+                ingredient.setMax(q);
+                stock_max.setText("Max: " + ingredient.getMax());
+            }
+        });
 
     }
 }
